be52f9c9f1858f0e6aca7e0eaf651368661828b6
v.nabatov@solar.local <v.nabatov@rt-solar.ru>
2023-10-02 19:32:17 +0300

    Navigation updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -143,9 +143,11 @@ export default function RootLayout({children}: RootLayoutProps) {
             <header>
                 <div className="flex items-center justify-between">
                     <ModeToggle/>
-                    <nav className="ml-auto text-sm font-medium space-x-6">
-                        <Link href="/">Home</Link>
-                        <Link href="/about">About</Link>
+                    <nav className="ml-auto text-sm font-medium">
+                        <div className="space-x-6 align-middle mt-[3px]">
+                            <Link className="inline-block h-full" href="/">Home</Link>
+                            <Link className="inline-block h-full" href="/about">About</Link>
+                        </div>
                     </nav>
                 </div>
             </header>
